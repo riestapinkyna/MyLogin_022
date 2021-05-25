68b8f571afcfc5ae637f85e82d09e8200db7b75e
Irza Muhammad Fatir <0320200012@polman.astra.ac.id>
2021-05-25 15:22:09 +0700

    form 1 pink

--- MyLogin_022/Form1.Designer.cs
+++ MyLogin_022/Form1.Designer.cs
@@ -96,7 +96,7 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(61, 158);
+            this.label4.Location = new System.Drawing.Point(34, 158);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(175, 20);
             this.label4.TabIndex = 6;
